63bec8fd2dabc809a7eb2c904eec800331054fe8
Sushmitha2708 <51853078+Sushmitha2708@users.noreply.github.com>
2019-06-16 10:50:40 +0530

    Delete sample.js

--- sample.js
+++
@@ -1,6 +0,0 @@
-var http = require ("http");
-http.createServer( function(request, response){
-	response.writeHead(200,{'content-Type':'text/plain'});
-	response.end('hello world\n');
-
-}).listen(8080);
